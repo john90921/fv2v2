e4e09a4f95f819dbe752e29034974842034f697e
john90921 <sorvictor90@gmail.com>
2025-10-31 20:03:24 +0800

    filter_ui_update

--- lib/views/pages/components/search/showFilterDialog.dart
+++ lib/views/pages/components/search/showFilterDialog.dart
@@ -20,77 +20,84 @@ Future<Filter?> showFilterDialog({
           return AlertDialog(
             shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
             title: const Text("Select Filter"),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text("Date Range"),
-                const SizedBox(height: 10),
-                DropdownButton<String>(
-                  isExpanded: true,
-                  value: selectedDate,
-                  items: <String>["today", "week", "month", "year"]
-                      .map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  onChanged: (newValue) {
-                    setState(() {
-                      selectedDate = newValue!;
-                    });
-                  },
-                ),
-                const SizedBox(height: 20),
-                const Text("Sort By"),
-                const SizedBox(height: 10),
-                DropdownButton<String>(
-                  isExpanded: true,
-                  value: selectedSortBy,
-                  items: <String>['latest', 'popular']
-                      .map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  onChanged: (newValue) {
-                    setState(() {
-                      selectedSortBy = newValue!;
-                    });
-                  },
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context).pop(
-                          Filter(
-                            date: selectedDate.toLowerCase(),
-                            sortBy: selectedSortBy.toLowerCase(),
-                          ),
-                        );
-                      },
-                      child: const Text("Apply"),
+            content: SafeArea(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("Date Range"),
+                  const SizedBox(height: 10),
+                  DropdownButton<String>(
+                    isExpanded: true,
+                    value: selectedDate,
+                    items: <String>["today", "week", "month", "year"]
+                        .map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (newValue) {
+                      setState(() {
+                        selectedDate = newValue!;
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  const Text("Sort By"),
+                  const SizedBox(height: 10),
+                  DropdownButton<String>(
+                    isExpanded: true,
+                    value: selectedSortBy,
+                    items: <String>['latest', 'popular']
+                        .map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (newValue) {
+                      setState(() {
+                        selectedSortBy = newValue!;
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width: double.infinity,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).pop(
+                              Filter(
+                                date: selectedDate.toLowerCase(),
+                                sortBy: selectedSortBy.toLowerCase(),
+                              ),
+                            );
+                          },
+                          child: const Text("Apply"),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).pop(Filter.initial());
+                          },
+                          child: const Text("Reset"),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text("Cancel"),
+                        ),
+                      ],
                     ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context).pop(Filter.initial());
-                      },
-                      child: const Text("Reset"),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: const Text("Cancel"),
-                    ),
-                  ],
-                ),
-              ],
+                  ),
+                ],
+              ),
             ),
           );
         },
